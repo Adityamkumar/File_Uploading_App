ffa9244e00ca9f60dc0968c4e386e78228ea2254
Adityamkumar <adityasharma841460@gmail.com>
2026-01-17 22:58:40 +0530

    fix: simplify CORS origin handling and improve error callback logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,29 +11,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 
-const isProd = process.env.NODE_ENV === "production";
-
-const allowedOrigins = isProd
-  ? [process.env.CLIENT_URL_PROD]
-  : [process.env.CLIENT_URL_DEV];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://droply-63.vercel.app"
+];
 
 app.use(
   cors({
     origin: function (origin, callback) {
+      // allow Postman, curl, server-to-server
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS: " + origin));
+      return callback(null, false);
     },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    credentials: true
   })
-);
-
+)
 app.options(/.*/, cors());
 
 
